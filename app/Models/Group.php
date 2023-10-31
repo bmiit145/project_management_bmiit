2b2fb29150dd3ce262e3a851b24ab7c7af71c6b8
bmiit145 <21bmiit145@gmail.com>
2023-11-01 01:14:35 +0530

    create a migration groups

---
+++ app/Models/Group.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'created_by',
+        'updated_by',
+    ];
+}
---
+++ database/migrations/2023_10_31_192401_create_student_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_groups', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('studentenro');
+            $table->foreignId('groupid')->constrained("groups");
+            $table->foreignId("courseYearId")->constrained("course_years");
+            $table->foreign('studentenro')->references("enro")->on("students");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_groups');
+    }
+}
