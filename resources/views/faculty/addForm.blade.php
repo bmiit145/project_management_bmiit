0c7c3e0f35399774f6d958b887b511bde5f7f4ff
bmiit145 <21bmiit145@gmail.com>
2023-11-10 15:56:30 +0530

    feture date problem solve

--- resources/views/faculty/addForm.blade.php
+++ resources/views/faculty/addForm.blade.php
@@ -32,9 +32,9 @@
                                         class="bx bx-user"></i></span>
                                 <!-- <span class="input-group-text">First and last name</span> -->
                                 <input type="text" name="fname" placeholder="John" aria-label="First name"
-                                    class="form-control">
+                                       class="form-control">
                                 <input type="text" name="lname" placeholder="Doe" aria-label="Last name"
-                                    class="form-control">
+                                       class="form-control">
 
                             </div>
                             <label id="fname-error" class="error" for="fname" style="display:none ;"></label><br>
@@ -46,22 +46,22 @@
                                 <span id="designation2" class="input-group-text">
                                     <i class="bx bx-buildings"></i></span>
                                 <input type="text" id="designation" class="form-control"
-                                    name="designation" placeholder="Assosiate professor"
-                                    aria-label="Assosiate professor" aria-describedby="designation2" />
+                                       name="designation" placeholder="Assosiate professor"
+                                       aria-label="Assosiate professor" aria-describedby="designation2"/>
                             </div>
-                          </div>
-                          <label id="designation-error" class="error" for="designation"></label>
+                        </div>
+                        <label id="designation-error" class="error" for="designation"></label>
                         <div class="mb-3">
                             <label class="form-label" for="email">Email</label>
                             <div class="input-group input-group-merge">
                                 <span class="input-group-text"><i class="bx bx-envelope"></i></span>
                                 <input type="text" id="email" class="form-control" name="email"
-                                    placeholder="john.doe" aria-label="john.doe"
-                                    aria-describedby="email2" />
+                                       placeholder="john.doe" aria-label="john.doe"
+                                       aria-describedby="email2"/>
                                 <span id="email2" class="input-group-text">@utu.ac.in</span>
                             </div>
                             <!-- <div class="form-text">You can use letters, numbers & periods</div> -->
-                            <div class="form-text">Username is same as email for login </div>
+                            <div class="form-text">Username is same as email for login</div>
                             <label id="email-error" class="error" for="email"></label>
                         </div>
                         <div class="mb-3">
@@ -70,8 +70,8 @@
                                 <span id="contactno" class="input-group-text"><i
                                         class="bx bx-phone"></i></span>
                                 <input type="text" id="contactno" class="form-control phone-mask"
-                                    name="contactno" placeholder="658 799 8941" aria-label="658 799 8941"
-                                    aria-describedby="contactno" />
+                                       name="contactno" placeholder="658 799 8941" aria-label="658 799 8941"
+                                       aria-describedby="contactno"/>
                             </div>
                             <label id="contactno-error" class="error" for="contactno"></label>
                         </div>
@@ -81,8 +81,8 @@
                                 <!-- <span id="basic-icon-default-message2" class="input-group-text"
                             ><i class="bx bx-comment"></i
                               ></span> -->
-                                <input class="form-control" name="doj" type="date" value="2022-06-18"
-                                    id="html5-date-input">
+                                <input class="form-control" name="doj" type="date" value="<?php echo date('Y-m-d'); ?>"
+                                       id="html5-date-input"  max="<?php echo date('Y-m-d'); ?>" >
                                 <!-- <textarea id="basic-icon-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?"
                                     aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea> -->
                             </div>
@@ -100,7 +100,7 @@
 <script src="{{ asset('js/jquery.validate.js') }}"></script>
 
 <script>
-    $.validator.addMethod("noAtSymbol", function(value, element) {
+    $.validator.addMethod("noAtSymbol", function (value, element) {
         return value.indexOf("@") === -1;
     }, "Email should not contain @ symbol");
 
@@ -128,7 +128,9 @@
                 digits: true // Only allow numeric digits
             },
             "doj": {
-
+                // can't be future date
+                required: true,
+                date:true,
             },
         },
         messages: {
@@ -138,19 +140,23 @@
                 maxlength: "Contact number must be exactly 10 digits long",
                 digits: "Please enter only numeric digits"
             },
-            fname:{
-              required: "Please enter your First name",
+            fname: {
+                required: "Please enter your First name",
             },
-            lname:{
-              required: "Please enter your Last name",
+            lname: {
+                required: "Please enter your Last name",
             },
-            designation:{
-              required:"please Enter Designation",
+            designation: {
+                required: "please Enter Designation",
             },
-            email:{
-              required:"Please Enter Valid Info."
+            email: {
+                required: "Please Enter Valid Info."
             },
-            
+            doj: {
+                required: "Please Enter Date of Joining",
+                date: "Please Enter Valid Date",
+            }
+
         },
         // errorPlacement: function(error, element) {
         //     // Use Toastr to show error messages
@@ -160,16 +166,16 @@
         //     // Use Toastr to show a summary error message
         //     toastr.error('Please fix the highlighted fields');
         // },
-        submitHandler: function(form , event) {
+        submitHandler: function (form, event) {
             // This function will be executed when the form is valid
             // You can perform any custom actions here, such as AJAX submission
             // form.submit();
             event.preventDefault();
             oldemail = $('#email').val();
             console.log(oldemail);
-            newemail = oldemail+'@utu.ac.in';
+            newemail = oldemail + '@utu.ac.in';
             var formData = $('#addFacultyForm').serialize()
-            formData = formData.replace('email='+oldemail, 'email='+newemail)
+            formData = formData.replace('email=' + oldemail, 'email=' + newemail)
             $.ajax({
                 type: "post",
                 url: "{{route('faculty.add')}}",
