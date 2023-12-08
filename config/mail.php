3f4fc10cc56b5a4542ad2d6ac6c06a8f6c7e4427
bmiit145 <21bmiit145@gmail.com>
2023-12-09 01:06:41 +0530

    mail complate ith attahment

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 120,
             'auth_mode' => null,
         ],
 
