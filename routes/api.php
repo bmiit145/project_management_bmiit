41f5967658e713b2eb5cffa3f3db1ec23a3569e5
bmiit145 <21bmiit145@gmail.com>
2023-12-11 11:32:50 +0530

    api index

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// index function for vercel
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the API']);
+});
+
