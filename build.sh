f17f4fe0eb7e825590157778335797c749177ce6
PRIYANK SUTARIYA <103820970+bmiit145@users.noreply.github.com>
2024-07-02 04:20:58 +0000

    change build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Install dependencies using Docker
-docker run --rm -v $(pwd):/app -w /app php:7.4-cli bash -c "apt-get update && apt-get install -y libssl1.0.0 && composer install"
+# Install Composer dependencies
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install
