80461a80d4e020736628ae38024881a3ca413112
PRIYANK SUTARIYA <103820970+bmiit145@users.noreply.github.com>
2024-07-02 04:25:04 +0000

    change vercel.json

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Install Composer (if not already installed)
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+if [ $RESULT -ne 0 ]
+then
+    >&2 echo 'ERROR: Composer installation failed'
+    exit $RESULT
+fi
+
 # Install Composer dependencies
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
+php composer.phar install --no-dev --no-scripts --ignore-platform-reqs
